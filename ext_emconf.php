c7c5d463fcb5c469476ef908000e2be87305d17d
Mike Street <mike@liquidlight.co.uk>
2024-10-17 12:26:45 +0100

    [FEATURE] Specify TYPO3 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,9 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => 1,
     'version' => '',
     'constraints' => [
-        'depends' => [],
+        'depends' => [
+            'typo3' => '11.5.0-11.5.99',
+        ],
         'conflicts' => [],
         'suggests' => [],
     ],
